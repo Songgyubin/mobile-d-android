d311b46690e3da36cd736e4a5db9bf9229970ef4
DESKTOP-BQT0SBO\thd04 <thd0427__@naver.com>
2021-08-14 21:04:52 +0900

    Initial Project

---
+++ app/src/main/java/op/gg/joinus/login/LoginActivity.kt
@@ -0,0 +1,16 @@
+package op.gg.joinus.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import op.gg.joinus.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_JoinUs)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}

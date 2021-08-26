826162fd72ab6cfdd45be75d3747591bef09be58
DESKTOP-BQT0SBO\thd04 <thd0427__@naver.com>
2021-08-27 00:51:35 +0900

    google login setting

--- app/src/main/java/op/gg/joinus/login/LoginActivity.kt
+++ app/src/main/java/op/gg/joinus/login/LoginActivity.kt
@@ -2,15 +2,29 @@ package op.gg.joinus.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import op.gg.joinus.R
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var googleSignInOptions: GoogleSignInOptions
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_JoinUs)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        initLogin()
+    }
+
+    private fun initLogin() {
+        googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail() // email addresses도 요청함
+            .build();
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
     }
 
 }
